2e9784d4ce2a0cd520a6f1b8fe1fb010d8a55d1f
Taehwan Kim <obtusa07@gmail.com>
2023-07-17 15:11:05 +0900

    Feat: SpringBoot Setup

---
+++ src/main/java/com/obtusa07/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -0,0 +1,12 @@
+package com.obtusa07.springbootdeveloper;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDeveloperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDeveloperApplication.class, args);
+    }
+}
