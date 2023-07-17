032f139ee3f18d83398507cbb1a613a4c35e222c
Taehwan Kim <obtusa07@gmail.com>
2023-07-17 15:50:59 +0900

    Feat: add BlogRepository

--- src/main/java/com/obtusa07/springbootdeveloper/BlogRepository/BlogRepository.java
+++ src/main/java/com/obtusa07/springbootdeveloper/BlogRepository/BlogRepository.java
@@ -1,2 +1,8 @@
-package com.obtusa07.springbootdeveloper.BlogRepository;public class BlogRepository {
+package com.obtusa07.springbootdeveloper.BlogRepository;
+
+import com.obtusa07.springbootdeveloper.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// JPA레포지토리가 제공하는 메서드를 사용하기 위함
+public interface BlogRepository extends JpaRepository<Article, Long> {
 }
