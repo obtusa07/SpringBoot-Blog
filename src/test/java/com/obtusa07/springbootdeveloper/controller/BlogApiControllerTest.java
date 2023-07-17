62fa4badbdf2ba26d017e134020383632d21b055
Taehwan Kim <obtusa07@gmail.com>
2023-07-17 18:40:32 +0900

    Feat: add Blog API test Controller

--- src/test/java/com/obtusa07/springbootdeveloper/controller/BlogApiControllerTest.java
+++ src/test/java/com/obtusa07/springbootdeveloper/controller/BlogApiControllerTest.java
@@ -1,8 +1,40 @@
 package com.obtusa07.springbootdeveloper.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.obtusa07.springbootdeveloper.BlogRepository.BlogRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-
+@SpringBootTest
+@AutoConfigureMockMvc // MockMvc 생성 및 자동 구성
 class BlogApiControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper; // 직렬화, 역직렬화를 위한 클래스
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    BlogRepository blogRepository;
+
+    @BeforeEach // 테스트 실행 전 실행하는 메서드
+    public void mockMvcSetUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .build();
+        blogRepository.deleteAll();
+    }
+
 
 }
